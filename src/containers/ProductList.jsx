cf20474e18a69f19852ce0b9ee5e5af4cf0ec770
Leandro Diaz <85961824+Leonidas111@users.noreply.github.com>
2022-03-24 17:14:35 -0500

    Local MAIN: migration template checkout from pages

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -3,13 +3,14 @@ import ProductItem from '@components/ProductItem';
 import useGetProducts from '@hooks/useGetProducts';
 import styles from '@styles/ProductList.module.scss';
 
-const API = process.env.API_URL;
+
+const API = 'https://api.escuelajs.co/api/v1/products';
 
 const ProductList = () => {
 	const products = useGetProducts(API);
 
 	return (
-		<section className={styles.ProductListModule}>
+		<section className={styles['main-container']}>
 			<div className={styles.ProductList}>
 				{products.map(product => (
 					<ProductItem product={product} key={product.id} />
