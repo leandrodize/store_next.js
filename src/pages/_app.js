2a224dfd8fafa516239746670c04cf4a98d226df
Leandro Diaz <85961824+Leonidas111@users.noreply.github.com>
2022-03-28 16:02:13 -0500

    Local MAIN: reapare bugs

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,20 @@
-import '@styles/globals.css';
 import AppContext from '@context/AppContext';
+import Script from 'next/script';
 import useInitialState from '@hooks/useInitialState';
-import Header from "@components/Header";
-
+import Header from '@components/Header';
+import '@styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   const initialState = useInitialState();
   return (
     <AppContext.Provider value={initialState}>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-SS9Z8X1JJ4" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-SS9Z8X1JJ4');`}
+      </Script>
       <Header />
       <Component {...pageProps} />
     </AppContext.Provider>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,22 +5,15 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <title>React Shop</title>
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-SS9Z8X1JJ4"></script>
-          <script dangerouslySetInnerHTML={{
-            __html: `  window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-SS9Z8X1JJ4');`
-          }}/>
+          <meta charSet="utf-8" />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
-};
+}
 
 export default MyDocument;
